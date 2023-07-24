1ab082f2c54f3dc114e616f4cc60429ce24bd0ef
maxbond <ceo@rf.ru>
2023-07-24 21:19:00 +0300

    dotenv added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config({ debug: false, override: true })
 // const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
 
